dc77613f3dbf76324149b4812c59217dbae94d7c
Jared Short <jshort@trek10.com>
2018-09-17 10:35:05 -0400

    conditional directives, default directive transformer

--- src/index.js
+++ src/index.js
@@ -6,21 +6,38 @@ const {ModelConnectionTransformer} = require('graphql-connection-transformer');
 const {ModelAuthTransformer} = require('graphql-auth-transformer');
 const {SearchableModelTransformer} = require('graphql-elasticsearch-transformer');
 const {AppSyncTransformer} = require('graphql-appsync-transformer');
+const {VersionedModelTransformer} = require('graphql-versioned-transformer');
+const {DefaultValueTransformer} = require('graphql-default-value-transformer');
 const fs = require('fs-extra');
 
 // Todo: actual cli parsing would be good
 const schemaPath = process.argv[2];
 const schema = fs.readFileSync(schemaPath, 'utf-8');
 
+const transformers = [
+    new AppSyncTransformer(),
+    new DynamoDBModelTransformer(),
+    new ModelConnectionTransformer(),
+    new VersionedModelTransformer(),
+    new DefaultValueTransformer()
+]
+
+// Only insert certain plugins if needed, otherwise they pollute the cloudformation
+// for no good reason.
+// We determine if needed with a simple string search for the directive string
+function conditionalInsertDirectivePlugin(plugin, searchString){
+    if (schema.indexOf(searchString) >= 0) {
+        transformers.push(plugin)
+    }
+}
+
+conditionalInsertDirectivePlugin(new ModelAuthTransformer(), '@auth');
+conditionalInsertDirectivePlugin(new SearchableModelTransformer(), '@searchable');
+
 // console.log(process.argv)
 const transformer = new GraphQLTransform({
-    transformers: [
-        new AppSyncTransformer(),
-        new DynamoDBModelTransformer(),
-        new ModelAuthTransformer(),
-        new ModelConnectionTransformer(),
-        new SearchableModelTransformer()
-    ]
+    transformers: transformers
 })
+
 const cfdoc = transformer.transform(schema);
 console.log(JSON.stringify(cfdoc, null, 2))
